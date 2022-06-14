7bf963452227c17f61fe7a475c18384976e94a01
jas <yasmine.derrag@outlook.fr>
2022-06-14 17:59:47 +0200

    db

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,11 @@
 Review.destroy_all
 Garden.destroy_all
 User.destroy_all
-
+Favorite.destroy_all
+GardenProduct.destroy_all
+GardenProductCharacteristic.destroy_all
+Product.destroy_all
+Characteristic.destroy_all
 
 user1= User.create!(last_name: "lolo", first_name: "david", address: "32 av  ggggg", email: "jjj@live.fr", password: "azerty")
 p "user create"
@@ -16,3 +20,8 @@ garden1= Garden.create!(name: "Fleur", address: "maier 5, nice", user_id: user1.
 p "garden create"
 review1= Review.create!(rating: "2", comment: "bon", user_id: user1.id, garden_id: garden1.id)
 p "review create"
+favorite1= Favorite.create!(garden_id: garden1.id, user_id: user1.id)
+characteristic1= characteristic.create!(name: "bio")
+product1= Product.create!(name: "tomate", category: "vegetable")
+gardenProductCharacteristic1= GardenProductCharacteristic.create!(gardeProduct_id: gardenProduct1.id, characteristic_id: characteristic1.id )
+gardenProduct1= GardenProduct.create!(product_id: product1.id, garden_id: garden1.id, quantity: "350")
