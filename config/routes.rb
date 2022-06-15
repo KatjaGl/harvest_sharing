c4febdb4c5bfb741d305bd67f9a946aa8ce1c619
Anne-sophie Monville <annesophiemonville@gmail.com>
2022-06-15 14:51:08 +0200

    controller fait et testé

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :products, only: [ :index, :show ]
-  resources :chatrooms, only: :show do
-    resources :messages, only: :create
+  resources :gardens, only: [:index, :show] do
+    resources :reviews, only: [:new, :create]
+    resources :favorites, only: [:new, :create]
   end
-  get "products/:id", to: "products#show", as: :product
-  get "products/new", to: "products#new"
-  post "products", to: "products#create"
-
+  resources :chatrooms, only: [:show, :create, :index]
 end
-  # resources :messages
-  # resources :gardens
-  # resources :garden_products
-  # resources :characteristics
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-# resources :reviews
-# resources :favorites
-# only: [:index, :show]do
-# resources
