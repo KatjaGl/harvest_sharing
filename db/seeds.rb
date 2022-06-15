3f6b3b938fa31d3de380280e10957029681306c2
jas <yasmine.derrag@outlook.fr>
2022-06-15 11:02:49 +0200

    « test »

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,23 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Review.destroy_all
-Garden.destroy_all
-User.destroy_all
 Favorite.destroy_all
+p "favdestroy"
+Review.destroy_all
+p "review destroy"
 GardenProduct.destroy_all
+p "gp destroy"
+Garden.destroy_all
+p "gar destroy"
 GardenProductCharacteristic.destroy_all
+p "gpc destroy"
 Product.destroy_all
+p "pro destroy"
 Characteristic.destroy_all
+p "cha destroy"
+User.destroy_all
+p "user destroy"
 
-user1= User.create!(last_name: "lolo", first_name: "david", address: "32 av  ggggg", email: "jjj@live.fr", password: "azerty")
+user1 = User.create!(last_name: "lolo", first_name: "david", address: "32 av  ggggg", email: "jjj@live.fr", password: "azerty")
 p "user create"
-garden1= Garden.create!(name: "Fleur", address: "maier 5, nice", user_id: user1.id)
+garden1 = Garden.create!(name: "Fleur", address: "maier 5, nice", user_id: user1.id)
 p "garden create"
-review1= Review.create!(rating: "2", comment: "bon", user_id: user1.id, garden_id: garden1.id)
+review1 = Review.create!(rating: "2", comment: "bon", user_id: user1.id, garden_id: garden1.id)
 p "review create"
-favorite1= Favorite.create!(garden_id: garden1.id, user_id: user1.id)
-characteristic1= characteristic.create!(name: "bio")
-product1= Product.create!(name: "tomate", category: "vegetable")
-gardenProductCharacteristic1= GardenProductCharacteristic.create!(gardeProduct_id: gardenProduct1.id, characteristic_id: characteristic1.id )
-gardenProduct1= GardenProduct.create!(product_id: product1.id, garden_id: garden1.id, quantity: "350")
+favorite1 = Favorite.create!(garden_id: garden1.id, user_id: user1.id)
+characteristic1 = Characteristic.create!(name: "bio")
+product1 = Product.create!(name: "tomate", category: "vegetable")
+gardenProduct1 = GardenProduct.create!(product_id: product1.id, garden_id: garden1.id, quantity: "350")
+gardenProductCharacteristic1 = GardenProductCharacteristic.create!(garden_product_id: gardenProduct1.id, characteristic_id: characteristic1.id )
