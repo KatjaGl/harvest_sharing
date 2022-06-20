b3ea8f12af98214864d05fe73511bc6659b9c26f
Emmanuelle MAPELLI <emmanuellemapelli@gmail.com>
2022-06-20 14:23:04 +0200

    ajout de la 1ere partie de geolocalisation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_14_105226) do
+ActiveRecord::Schema.define(version: 2022_06_20_112241) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,8 @@ ActiveRecord::Schema.define(version: 2022_06_14_105226) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_gardens_on_user_id"
   end
 
