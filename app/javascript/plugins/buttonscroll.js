81314b7b79c36472811bec2fc24c783b2fbe869b
jas <yasmine.derrag@outlook.fr>
2022-06-23 18:32:40 +0200

    push

--- app/javascript/plugins/buttonscroll.js
+++ app/javascript/plugins/buttonscroll.js
@@ -1,29 +1,17 @@
-/*const btnScrollToTop = () => {
+const btnScrollToTop = () => {
   const scrollbtnToTop = document.querySelector("#btnScrollToTop");
   if (scrollbtnToTop) {
-    scrollbtnToTop.addEventListener("click", function () {
-    window.scrollTo({
-      top: 0,
-      left: 0,
-      behavior: "smooth"
+    $(window).scroll(function(){
+      if ($(this).scrollTop() > 100) {
+          $('#btnScrollToTop').fadeIn();
+      } else {
+          $('#btnScrollToTop').fadeOut();
+      }
     });
-    // $("html, body").animate({ scrollTop: 0}, "slow");
-  });
-
+    $('#btnScrollToTop').click(function(){
+      $("html, body").animate({ scrollTop: 0 }, 600);
+      return false;
+    });
   }
-
 }
-  export { btnScrollToTop };
-*/
-
-$(window).scroll(function(){
-  if ($(this).scrollTop() > 100) {
-      $('#btnScrollToTop').fadeIn();
-  } else {
-      $('#btnScrollToTop').fadeOut();
-  }
-});
-$('#btnScrollToTop').click(function(){
-  $("html, body").animate({ scrollTop: 0 }, 600);
-  return false;
-});
+ export { btnScrollToTop };
